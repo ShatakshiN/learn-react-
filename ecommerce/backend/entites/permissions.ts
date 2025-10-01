44fafc16233659aba100a82fe7580d350afd529f
ShatakshiN <shatakshinimare27@gmail.com>
2025-10-01 16:41:02 +0530

    permission entity

---
+++ ecommerce/backend/entites/permissions.ts
@@ -0,0 +1,21 @@
+import { timeStamp } from "console";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name:"permissions"})
+export class Permissions{
+    @PrimaryGeneratedColumn()
+    id!:number
+
+    @Column({type:"varchar", length:50 , nullable:false})
+    permission_name!: string; 
+
+    @Column({type:"varchar",length:100, nullable:false})
+    description!:string;
+
+    @CreateDateColumn({type:"timestamp"})
+    createdAt!: Date;
+
+    @UpdateDateColumn({type:"timestamp"})
+    updatedAt!: Date;
+
+}
