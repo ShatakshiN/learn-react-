fc48675a90874129e82d44fb2e563f328da2354c
ShatakshiN <shatakshinimare27@gmail.com>
2025-10-01 17:47:42 +0530

    attributes table

---
+++ ecommerce/backend/entites/attributes.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("attributes")
+export class Attribute {
+  @PrimaryGeneratedColumn()
+  id!: number;   
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  attribute!: string;  
+}
