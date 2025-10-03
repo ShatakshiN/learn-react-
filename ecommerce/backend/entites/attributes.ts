dcd7ddc449da15a277a3148d8f4ce332b318ad84
ShatakshiN <shatakshinimare27@gmail.com>
2025-10-03 16:30:48 +0530

    attributes entity

--- ecommerce/backend/entites/attributes.ts
+++ ecommerce/backend/entites/attributes.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { VariantAttributeValue } from "./variantAttributeValues.js";
 
 @Entity("attributes")
 export class Attribute {
@@ -8,6 +9,8 @@ export class Attribute {
   @Column({ type: "varchar", length: 100, nullable: false })
   attribute!: string;  
 
+  @OneToMany(() => VariantAttributeValue, (vav) => vav.attribute)
+  variantValues!: VariantAttributeValue[]
   
 }
 
